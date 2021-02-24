1281afe486bcc94f5bb9a43bbcc5bb1b0982d0c3
devsgnr <ei.watila@gmail.com>
2021-02-24 23:22:34 +0000

    Modified to display UI and used react-query

--- src/components/user.jsx
+++ src/components/user.jsx
@@ -1,4 +1,4 @@
-import { useQuery, useQueryClient } from 'react-query';
+import { useQuery } from 'react-query';
 import { Button, Input, List, Avatar } from 'antd';
 import { useState } from 'react';
 import axios from 'axios';
@@ -8,8 +8,7 @@ function User() {
     const voterUrl = "https://api-demo-voting.herokuapp.com/addVoters";
 
     const [voterEmail, setVoterEmail] = useState('');
-  
-    const queryClient = useQueryClient();
+
   
     const { isLoading, isError, data, error } = useQuery('users', async () => {
       const { data } = await axios.get(baseUrl);
@@ -28,7 +27,7 @@ function User() {
     }
 
     if(isLoading) {
-      return <span>fetching new lists...</span>
+      return <span>loading list of voters...</span>
     }
   
     if(isError) {
@@ -38,30 +37,29 @@ function User() {
     return (
       <div>
         <div className="App">
-            <div>
-                <Input onChange={(e) => setVoterEmail(e.target.value)}></Input>
-                <Button type="primary" onClick={addVoter}>Send Email</Button>
-            </div>
+          <div>
+            <p>Send your email to the Voter database, and you'll recieve an email notification (~100% real)</p>
+            <Input placeholder="Enter email address" onChange={(e) => setVoterEmail(e.target.value)}></Input>
+            <Button className="mt-2" type="primary" onClick={addVoter}>Send Email</Button>
+          </div>
 
-            <div className="mt-3">
-                <List
-                    header={<div>Voters</div>}
-                    bordered
-                    dataSource={data.voters}
-                    renderItem={item => (
-                        <List.Item key={item.id}>
-                            <List.Item.Meta
-                            avatar={
-                                <Avatar />
-                            }
-                            title={<a href={`mailto:${item.email}`}>{item.email}</a>}
-                            description={item.email}
-                            />
-                            <div>hasVoted ~ {new String(item.hasVoted)}</div>
-                        </List.Item>
-                    )}
+          <div className="mt-3 pb-5">
+            <List
+              header={<div>Voters</div>}
+              bordered
+              dataSource={data.voters}
+              renderItem={item => (
+              <List.Item key={item.id}>
+                <List.Item.Meta 
+                  avatar={ <Avatar /> }
+                  title={<a href={`mailto:${item.email}`}>{item.email}</a>}
+                  description={item.email}
                 />
-            </div>
+                <div>{String(item.hasVoted)}</div>
+              </List.Item>
+              )}
+            />
+          </div>
         </div>
       </div>
     )
