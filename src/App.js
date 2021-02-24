bfcb2912cbf6ec38057215abb605716b058a23e0
devsgnr <ei.watila@gmail.com>
2021-02-24 14:28:39 +0000

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,36 @@
-import logo from './logo.svg';
+import { Button, Space, Card, Avatar } from 'antd';
+import axios from 'axios';
+import { useState } from 'react';
 import './App.css';
 
 function App() {
+
+  const url = "https://randomuser.me/api/";
+  const [payload, setPayload] = useState({ });
+  let updateVal = {}
+
+  const handleFetchUsers = () => {
+    axios.get(url)
+    .then((res) => {
+      updateVal.data = res.data.results[0];
+      setPayload({...payload, ...updateVal});
+    })
+    .then(e => {
+      console.log(e)
+    })
+  }
+
+  const user = payload;
+  console.log(user);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Button type="primary" onClick={handleFetchUsers}>Fetch Users</Button>
+      <div>
+        <code>
+          {JSON.stringify(payload, null, 5)}
+        </code>
+      </div>
     </div>
   );
 }
